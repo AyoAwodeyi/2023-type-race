b0488a346913461d8388dd0d69eb5fdda8958db0
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2023-07-07 13:02:41 -0500

    YES

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, fetchData  } from 'react';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn react
-        </a>
-      </header>
-    </div>
-  );
+
+const refresh = () => window.location.reload(true);
+  
+
+
+const buttonTextItems = [
+ 'Oh What a relief it is',
+ 'Omg it is a relief',
+ 'Oh man oh man it is something'];
+
+const initialgameState={
+
+victory:false,
+startTime: null,
+endTime:null,
 }
 
-export default App;
+// States
+ const[snippet, setSnippet] = useState('');
+  const [userText, setUserText] = useState('');
+const [gameState, setGameState] = useState(initialgameState);
+const [wins, setWins] = useState(null);
+const [hasError, setHasError] = useState(false);
+const [films,setFilms] = useState([]);
+
+////////////////////////////////
+
+
+
+
+
+
+
+
+  const updateUserText = (event) => {
+setUserText(event.target.value);
+
+if(event.target.value === snippet){
+setGameState({
+  ...gameState,
+  victory:true,
+   endTime:new Date().getTime() - gameState.startTime 
+});
+
+ }
+  };
+
+
+let timeInSecs = gameState.endTime /1000;
+
+
+
+
+
+
+
+
+
+ const  chooseSnippet = (index) => {
+setSnippet(buttonTextItems[index]);
+ setGameState({ ...gameState, startTime:new Date().getTime()});
+ }
+
+
+
+
+
+
+  return (
+    <div>
+       <h2 className='title' >Type Race</h2>      
+       <h3>'Click What You Want to Type From The List Below'</h3>
+       
+       <div> 
+{buttonTextItems.map((textItem,index)=> <button className='snippetOptions' onClick={() => chooseSnippet(index)}>{textItem}</button>)}
+       </div>
+       <div>{snippet} </div>
+       <input value ={userText} onChange={updateUserText}/> 
+    
+       <h4>{gameState.victory ? `You Win: ${timeInSecs} Secs`: "You Suck"}</h4>
+     <button onClick ={refresh}>Play Again</button>
+     
+        </div>
+
+
+
+
+
+
+
+
+
+  );
+};
+
+
+
+export default App; 
